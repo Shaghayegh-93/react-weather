0953c1a3d6839282fc953d4d44e49686a88316d9
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2022-10-12 23:36:10 +0330

    add some logic to weather component and css file of that

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Shaghayegh and is
+          <a href="https://github.com/Shaghayegh-93/react-weather" target={"_blank"}></a>
+        </footer>
       </div>
-      
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import "./weather.css";
 const Weather = () => {
   return (
-    <div>Weather</div>
-  )
-}
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            {" "}
+            <input
+              type={"search"}
+              placeholder="Enter a city.."
+              className="formControl"
+            />
+          </div>
+          <div className="col-3">
+            {" "}
+            <input type={"submit"} value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 20:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="cloudy"
+          />
+          13°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 7%</li>
+            <li> Humidity: 86%</li>
+            <li> Wind: 7 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Weather
+export default Weather;
